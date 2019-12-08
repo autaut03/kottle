62a54ca132bee8b749c90d2781dbf0c746f5f9d7
Alex Wells <autaut03@gmail.com>
2019-12-08 22:19:12 +0200

    Travis-CI integration test

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+LATEST_LOG_FILE="tests/integration/run/logs/latest.log"
+DEBUG_LOG_FILE="tests/integration/run/logs/debug.log"
+SUCCESS_MESSAGE="Kottle integration test was successfully loaded"
+
+rm -f "$LATEST_LOG_FILE"
+
+echo "Running client tests..."
+
+! ./gradlew :tests:integration:runClient
+
+if ! grep -q "$SUCCESS_MESSAGE" "$LATEST_LOG_FILE"; then
+  echo "Client tests failed. latest.log: "
+  echo "$(cat $LATEST_LOG_FILE)"
+  echo "debug.log"
+  echo "$(cat $DEBUG_LOG_FILE)"
+
+  exit 101
+fi
+
+rm -f "$LATEST_LOG_FILE"
+
+echo "Running server tests..."
+
+! ./gradlew :tests:integration:runServer
+
+if ! grep -q "$SUCCESS_MESSAGE" "$LATEST_LOG_FILE"; then
+  echo "Server tests failed. latest.log: "
+  echo "$(cat $LATEST_LOG_FILE)"
+  echo "debug.log"
+  echo "$(cat $DEBUG_LOG_FILE)"
+
+  exit 102
+fi
+
+echo "Success! All tests passed."
+
+exit 0
